083c531e673836ea00a5b947cc1963b0bd796d90
Hiếu Trương Vinh <73266030+top1taker@users.noreply.github.com>
2020-11-06 15:06:17 +0700

    Update load.js

--- load.js
+++ load.js
@@ -12,9 +12,15 @@ http.createServer(function (req, res) {
     userarr = arr.slice(start + 1, end);
     html = "<form action='server'><select name='username'>"
     for(let i = 0; i < userarr.length; i++) {
-      currentuser = userarr[i]
-      value = currentuser.slice(0, currentuser.indexOf(' '));	
-      html += "<option value='"+ value +"'>"+ currentuser+"</option>";
+      currentline = userarr[i].split("");
+      while(currentline.length > 0){
+      currentuser = currentline.splice(0, 25).join("");
+      value = currentuser.slice(0, currentuser.indexOf(' '));
+         if (value.length > 0) {	
+         html += "<option value='"+ value +"'>"+ currentuser+"</option>";
+         } 
+      }
+      
     }
     html += "</select><input type='submit' value='Choose'></form>";
     res.write(html);
