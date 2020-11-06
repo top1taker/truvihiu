3492196538d21ea9a06932fc5649ee72b8d4bcab
Hiếu Trương Vinh <73266030+top1taker@users.noreply.github.com>
2020-11-06 13:30:56 +0700

    Add files via upload

---
+++ load.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+http.createServer(function (req, res) {
+  //Open a file on the server and return its content:
+  fs.readFile('alluser.txt','utf-8', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    str = data.toString();    
+    arr = str.split('\n'); 
+    start = arr.findIndex(checkstart);
+    end = arr.findIndex(checkend);
+    userarr = arr.slice(start + 1, end);
+    html = "<form action='server'><select name='username'>"
+    for(let i = 0; i < userarr.length; i++) {
+      currentuser = userarr[i]
+      value = currentuser.slice(0, currentuser.indexOf(' '));	
+      html += "<option value='"+ value +"'>"+ currentuser+"</option>";
+    }
+    html += "</select><input type='submit' value='Choose'></form>";
+    res.write(html);
+    var q = url.parse(req.url, true).query;
+  var txt = q.username;
+  if (txt != null) console.log("You choose: " + txt + "\nPress 'Ctrl + C' and Type 'net user " + txt + "'");
+    return res.end();
+  });
+  
+}).listen(8080);
+
+function checkstart(str) {
+  return str.indexOf('-------') >= 0
+}
+
+function checkend(str) {
+  return str.indexOf('The command completed successfully') >=0;
+}
